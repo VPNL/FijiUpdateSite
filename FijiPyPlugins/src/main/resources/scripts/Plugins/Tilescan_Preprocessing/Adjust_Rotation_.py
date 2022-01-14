5aa5b4c18bf932830e87319cd96ef1ff331ac4d0
unknown <alexrezai.rezai@gmail.com>
2022-01-13 17:52:39 -0800

    finished first version of script

--- FijiPyPlugins/src/main/resources/scripts/Plugins/Tilescan_Preprocessing/Adjust_Rotation_.py
+++ FijiPyPlugins/src/main/resources/scripts/Plugins/Tilescan_Preprocessing/Adjust_Rotation_.py
@@ -11,8 +11,7 @@ displayed to the user so that they can use Fiji's rotation function to
 adjust the rotation of the image.
 
 INPUTS
-
-    - input_dir (java.io.File): Directory containing the separate
+     - input_dir (java.io.File): Directory containing the separate
 								channels of the image the user wanted to
                                 adjust the rotation.
 
@@ -41,8 +40,8 @@ from java.io import File
 inputDir = input_dir.getAbsolutePath()
 del input_dir
 
-# Import ImagePlus so we can read image files
-from ij import ImagePlus
+# Import ImagePlus so we can read image files and IJ so we can save them
+from ij import ImagePlus, IJ
 
 # Construct a dummy ImagePlus object. For some reason, you need to do
 # this before you can use the ImagePlus() construction to read image
@@ -53,6 +52,14 @@ ImagePlus()
 # images, and the ImageFiles module containing functions for searching
 # for image files.
 from ImageTools import ImageFiles, ImageProcessing
+
+# Import os so we can identify the file separator character used for
+# this computer
+import os
+
+# Import positive integer object from bio-formats so we can specify
+# image dimensions
+from ome.xml.model.primitives import PositiveInteger
 
 ########################################################################
 ############ OPEN THE IMAGE FILE WE WANT TO MANUALLY ROTATE ############
@@ -75,5 +82,64 @@ del img2Rotate
 maxProj2Rotate = img2RotateStack.maxProj()
 del img2RotateStack
 
+########################################################################
+######### IDENTIFY THE ANGLE THAT WE WANT TO ROTATE OUR IMAGES #########
+########################################################################
+
 # Ask the user to rotate this image, returning the angle of rotation
 [_,rotAngle] = ImageProcessing.manualRotation(maxProj2Rotate)
+
+# Search the directory specified by the user to identify the text file
+# containing the final angle of rotation of our images
+rotAngleTextFilePath = ImageFiles.findImgsInDir(inputDir,'txt',
+                                                'RotationInDegrees_')
+
+# Open the text file
+with open(rotAngleTextFilePath,'r+') as rotAngleTextFile:
+
+    # Read the angle currently saved in the text file
+    currRot = float(rotAngleTextFile.read())
+
+    # Set the current position in the file to the start
+    rotAngleTextFile.seek(0)
+
+    # Add our manual rotation angle with the current rotation and write
+    # final angle to text file
+    rotAngleTextFile.write(str(currRot + rotAngle))
+
+    # Close the file
+    rotAngleTextFile.close()
+del rotAngleTextFilePath, rotAngleTextFile, currRot
+
+########################################################################
+################## READ, ROTATE AND RESAVE ALL IMAGES ##################
+########################################################################
+
+# Get a list of all image files that we want to rotate
+imgFiles2Rotate = ImageFiles.findImgsInDir(inputDir,'tif','c\d_')
+
+# Rotate across all image files
+for imgFile2Rotate in imgFiles2Rotate:
+
+    # Read the image file
+    img2Rotate = ImagePlus(imgFile2Rotate)
+
+    # Rotate the image file
+    rotatedImg = ImageProcessing.autoRotation(img2Rotate,rotAngle)
+    del img2Rotate
+
+    # Get the meta data for this image
+    imgMetaData = ImageFiles.getOMEXMLMetadata(imgFile2Rotate)
+
+    # Update the dimensions of the image stored in the meta data to
+    # match our rotated image
+    imgMetaData.setPixelsSizeX(rotatedImg.getWidth(),0)
+    imgMetaData.setPixelsSizeY(rotatedImg.getHeight(),0)
+
+    # Delete the current image file so we can re-write it
+    os.remove(imgFile2Rotate)
+
+    # Save our rotated image with this meta data to the same image file
+    ImageFiles.saveCompressedImg(rotatedImg,imgMetaData,imgFile2Rotate)
+    rotatedImg.hide()
+    del rotatedImg, imgMetaData
