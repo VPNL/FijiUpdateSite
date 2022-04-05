d432eaf645301130b0ac60dfc3719226261ea297
aarezai <alexrezai.rezai@gmail.com>
2022-04-05 16:32:37 -0700

    adding function to read csv files into python dictionaries

--- FijiPyLib/src/main/resources/DataFiles.py
+++ FijiPyLib/src/main/resources/DataFiles.py
@@ -8,6 +8,10 @@ data files
 
         - Saves a python dictionary out to a csv file
 
+    csv2dict(csvPath)
+
+        - Reads a csv file to a python dictionary
+
 '''
 
 ########################################################################
@@ -56,3 +60,63 @@ def dict2csv(dataDict,outPath):
 
         # Close the csv file
         outfile.close()
+
+########################################################################
+############################### csv2dict ###############################
+########################################################################
+
+# Write a function to read csv files
+def csv2dict(csvPath):
+    '''
+    Reads a csv file to a python dictionary
+
+    csv2dict(csvPath)
+
+        - csvPath (String): File path to the location of the csv file
+                            you want to read into a dictionary
+
+    OUTPUTS a python dictionary
+    '''
+
+    # Open the csv file
+    with open(csvPath) as csvFile:
+
+        # Read the csv file row by row
+        csvReader = csv.reader(csvFile)
+
+        # Read the first row of the csv file
+        for row in csvReader:
+
+            # Store the list of all the column names in the csv file
+            cols = row
+
+            # End the loop as we only want to read the first row
+            break
+
+        # Initialize a python dictionary that will contain the contents
+        # of the csv file
+        dict = {}
+
+        # Loop across all of the column names in the csv file
+        for key in cols:
+
+            # Initialize a list under this key
+            dict[key] = []
+
+    # Re-open the csv file so we can read it with a different package 
+    with open(csvPath) as csvFile:
+
+        # Use the dict reader to read the csv file row by row
+        reader = csv.DictReader(csvFile)
+
+        # Loop across all rows of the csv file
+        for row in reader:
+
+            # Loop across all column titles
+            for key in row.keys():
+
+                # Append this value to our dictionary
+                dict[key].append(row[key])
+
+    # Return the final dictionary
+    return dict
